e976dcde6d769c23cd7a830169988c4b146b5fe7
fallen-zero <fallen_zero@163.com>
2024-08-09 15:18:16 +0800

    fit: update package.json build

--- src/pages/api/user.ts
+++ src/pages/api/user.ts
@@ -2,7 +2,7 @@
  * @Author       : fallen_zero
  * @Date         : 2024-08-08 15:09:35
  * @LastEditors  : fallen_zero
- * @LastEditTime : 2024-08-09 09:10:39
+ * @LastEditTime : 2024-08-09 15:17:21
  * @FilePath     : /earthworm/src/pages/api/user.ts
  * @FileName     :
  */
@@ -10,6 +10,6 @@ import prisma from '@lib/prisma';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function GET(req: NextApiRequest, res: NextApiResponse) {
-  const users = await prisma.user.findMany();
+  const users = await prisma.course.findMany();
   res.status(200).json({ code: 1, data: users, message: 'success' });
 }
